c6cc8a3d1758d36e69907d26bb8821a256f95362
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-08-25 17:25:12 +0530

    title update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,15 @@
+import Head from 'next/head'
 import './style.scss'
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <title>pureYet?</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
+
 }
