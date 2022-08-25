f72256ab2f8c7e74f715d4a7a437cacd49140b2d
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-08-25 17:26:35 +0530

    +ico

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ export default function MyApp({ Component, pageProps }) {
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <title>pureYet?</title>
+                <link rel="icon" href="https://raw.githubusercontent.com/akhilashokan/akhilashokan.github.io/react/public/favicon.ico" />
             </Head>
             <Component {...pageProps} />
         </>
