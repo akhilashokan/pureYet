8be659f08112b8114834dcedfc8b07efef565b4e
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2024-12-13 20:56:25 +0530

    feat: add apex chart options

--- components/Graph.tsx
+++ components/Graph.tsx
@@ -3,20 +3,49 @@ import dynamic from "next/dynamic";
 const ReactApexChart = dynamic(() => import("react-apexcharts"), { ssr: false }); // window not found fix | https://medium.com/@farrel.abyansyah/how-to-use-apexcharts-in-a-next-js-project-96e413bc9b31
 
 export default function Graph({ data }) {
-    console.log(data);
     const hourly = data?.hourly;
     const pm2_5 = hourly?.pm2_5
-    const options = {
-        series: [
-            {
-                name: 'pm2.5',
-                data: pm2_5
-            }
-        ],
-    }
+    const pm10 = hourly?.pm10
+    const time = hourly?.time
+    const series = [
+        {
+            name: 'pm2.5',
+            data: pm2_5
+        }, {
+            name: 'pm10',
+            data: pm10
+        }
+    ]
     return (
-        <div className="mt-2">
-            <ReactApexChart options={{ chart: { type: 'line' } }} series={options.series} height={350} width={400} />
+        <div className="mt-4">
+            <ReactApexChart options={{ ...options, labels: time }} series={series} height={350} />
         </div>
     )
+}
+
+const options: ApexCharts.ApexOptions = {
+    chart: {
+        type: 'area',
+        toolbar: {
+            show: false,
+        }
+    }, dataLabels: {
+        enabled: false
+    }, stroke: {
+        curve: 'smooth'
+    }, xaxis: {
+        type: 'datetime',
+        tickAmount: 10,
+        axisTicks: {
+            show: false
+        },
+        labels: {
+            format: 'dd MMM yy hh:mm'
+        }
+    }, tooltip: {
+        fillSeriesColor: true,
+        x: {
+            show: false
+        },
+    }
 }
